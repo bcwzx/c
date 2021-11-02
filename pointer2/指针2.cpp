c7d5eaac594b37b93e6a2ee8af7928c47fb1cfb5
bcwzx <2237455121@qq.com>
2021-11-02 21:33:05 +0800

    pointer2

---
+++ pointer2/指针2.cpp
@@ -0,0 +1,37 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+int main()
+{
+	//指针数组 -存放指针的数组
+//int main()
+//{
+//	int a = 10;
+//	int b = 20;
+//	int c = 30;
+//	int* arr2[3] = { &a, &b,&c };//指针数组
+//	int i = 0;
+//	for (i = 0; i < 3; i++)
+//	{
+//		printf("%d ", *(arr2[i]));
+//	}
+//
+//	return 0;
+//}
+
+
+
+
+
+
+
+//二重指针
+//int main()
+//{
+//	int a = 10;
+//	int* pa = &a;
+//	int** ppa = &pa;
+//	printf("%d\n", **ppa);
+//	return 0;
+//}
+
+}
