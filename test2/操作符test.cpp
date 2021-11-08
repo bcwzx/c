cf1afb6f27e915c59b87b03f52243160de48d9e2
bcwzx <2237455121@qq.com>
2021-11-08 21:10:32 +0800

    test2

--- test2/操作符test.cpp
+++ test2/操作符test.cpp
@@ -1,14 +1,81 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 #include<stdio.h>
 #include<stdlib.h>
+//使用指针打印数组内容，不使用数组下标，使用指针
+void print(int* p, int sz)
+{
+	int i = 0;
+	for (i = 0; i < sz; i++)
+	{
+		printf("%d ", *(p + i));
+	}
+}
 int main()
-{	
-
-
-
-
+{
+	int arr[] = { 1,2,3,4,5,6,7,8,9 };
+	int sz = sizeof(arr) / sizeof(arr[0]);
+	print(arr, sz);
 	return 0;
 }
+
+
+
+
+
+
+
+
+
+//获取一个二进制数的奇数位和偶数位
+//void print(int m)
+//{
+//	int i = 0;
+//	printf("奇数位:\n");
+//	for (i = 30; i >= 0; i -= 2)
+//	{
+//		printf("%d", (m >> i) & 1);
+//	}
+//	printf("\n");
+//	printf("偶数位:\n");
+//	for (i = 31; i >= 1; i -= 2)
+//	{
+//		printf("%d", (m >> i) & 1);
+//	}
+//	printf("\n");
+//}
+//int main()
+//{
+//	int m = 0;
+//	scanf("%d", &m);
+//	print(m);
+//	return 0;
+//}
+
+
+
+
+
+//int get_diff_bit(int m,int n)
+//{
+//int tmp = m ^ n;//异或
+//int count = 0;
+//while (tmp)
+//{
+//	tmp = tmp & (tmp - 1);
+//	count++;
+//}
+//return count;
+//}
+////求二进制中不同位的个数
+//int main()
+//{	
+//	int m = 0;
+//	int n = 0;
+//	scanf("%d%d", &m, &n);
+//	int count = get_diff_bit(m, n);
+//	printf("count=%d\n", count);
+//	return 0;
+//}
 
 
 
